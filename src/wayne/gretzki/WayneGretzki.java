ef9f11069264cf4af079901c51f0b8413b4dbe78
wizwa9381 <wizwa9381@7KZM6Y1.Student.UGDSB.ED>
2018-10-09 09:21:18 -0400

    Gretzki

---
+++ src/wayne/gretzki/WayneGretzki.java
@@ -0,0 +1,24 @@
+/*
+ * William Zwart
+ * 18/9/18
+ * This should display Wayne Gretzki's name and number from memory.
+ */
+package wayne.gretzki;
+
+/**
+ *
+ * @author wizwa9381
+ */
+public class WayneGretzki {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String wayne = "Wayne Gretzki";
+        double number = 99;
+        System.out.println(wayne + "'s number is " + 99);
+        // TODO code application logic here
+    }
+    
+}
